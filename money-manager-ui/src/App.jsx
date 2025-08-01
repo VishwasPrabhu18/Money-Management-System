ddbe9e937fa6abe2f77fe70eb00d075e01e0c402
VishwasPrabhu18 <prabhuvishwas18@gmail.com>
2025-08-01 19:20:02 +0530

    App context and routes added

--- money-manager-ui/src/App.jsx
+++ money-manager-ui/src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
+import Category from "./pages/Category";
+import Filter from "./pages/Filter";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to Money Manager</h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
 
-export default App
+      <Toaster />
+    </>
+  );
+};
+
+export default App;
